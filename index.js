9e94b6320272d80f8d00336ff6ee3fae0cf58a92
Woody Lin <wancheng.lin001@umb.edu>
2025-06-03 22:44:18 -0400

    chance of uploading multiple images

--- index.js
+++ index.js
@@ -4,76 +4,181 @@ const CronJob = require("cron").CronJob;
 const fs = require("fs");
 
 // for Twitter
-const { twitterClient } = require("./twitterClient.js")
+const { twitterClient } = require("./twitterClient.js");
 const { download } = require("./utilities");
 
 // for Instagram
-const { IgApiClient } = require('instagram-private-api');
-const { get } = require('request-promise');
+const { IgApiClient } = require("instagram-private-api");
+const { get } = require("request-promise");
+
+// Function to determine number of images to post based on probability
+const getImageCount = () => {
+    const random = Math.random() * 100; // Generate random number 0-100
+
+    if (random < 70) {
+        return 1; // 70% chance
+    } else if (random < 90) {
+        return 2; // 20% chance (70-90)
+    } else {
+        return 4; // 10% chance (90-100)
+    }
+};
+
+// Function to get multiple unique random images
+const getRandomImages = (count) => {
+    const uris = JSON.parse(fs.readFileSync("uris.json", "utf8"));
+    const selectedImages = [];
+    const usedIndices = new Set();
+
+    // Ensure we don't try to get more images than available
+    const maxCount = Math.min(count, uris.length);
+
+    while (selectedImages.length < maxCount) {
+        const randomIndex = Math.floor(Math.random() * uris.length);
+
+        // Only add if we haven't used this index before
+        if (!usedIndices.has(randomIndex)) {
+            usedIndices.add(randomIndex);
+            selectedImages.push({
+                uri: uris[randomIndex],
+                filename: uris[randomIndex].substring(
+                    uris[randomIndex].lastIndexOf("/") + 1
+                ),
+            });
+        }
+    }
+
+    return selectedImages;
+};
+
+// Function to download multiple images
+const downloadImages = async (images) => {
+    const directory = "./img";
+    const downloadPromises = images.map((image, index) => {
+        return new Promise((resolve, reject) => {
+            const filepath = `${directory}/${image.filename}`;
+            download(image.uri, filepath, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({
+                        uri: image.uri,
+                        filepath: filepath,
+                    });
+                }
+            });
+        });
+    });
+
+    return Promise.all(downloadPromises);
+};
 
 const tweet = async () => {
-  // read the uris.json file and randomly choose a image
-  const uris = JSON.parse(fs.readFileSync("uris.json", "utf8"));
-  const randomIndex = Math.floor(Math.random() * uris.length);
-  const uri = uris[randomIndex];
-  // const uri = "https://example.com/images/image_210.JPG";
+    // read the uris.json file and randomly choose a image
+    const uris = JSON.parse(fs.readFileSync("uris.json", "utf8"));
+    const randomIndex = Math.floor(Math.random() * uris.length);
+    const uri = uris[randomIndex];
+    // const uri = "https://example.com/images/image_210.JPG";
 
-  // name the image
-  const directory = "./img";
-  const filename = uri.substring(uri.lastIndexOf("/") + 1);
-  // console.log(filename);
-  // Output: "image_210.JPG"
-  const filepath = `${directory}/${filename}`;
+    // name the image
+    const directory = "./img";
+    const filename = uri.substring(uri.lastIndexOf("/") + 1);
+    // console.log(filename);
+    // Output: "image_210.JPG"
+    const filepath = `${directory}/${filename}`;
 
-  // download the image from my GitHub album
-  download(uri, filepath, async function(err){
-    try {
-      const mediaId = await twitterClient.v1.uploadMedia(filepath, {
-        mimeType: "image/jpeg"
-      });
-      console.log(mediaId);
-      await twitterClient.v2.tweet({
-        text: "#GIDLE #IDLE #여자아이들 #아이들 #女娃",
-        media: {
-          media_ids: [mediaId],
-          // get the id @G_I_DLE, https://tweeterid.com/
-          tagged_user_ids: ['967000437797761024'],
+    // download the image from my GitHub album
+    download(uri, filepath, async function (err) {
+        try {
+            const mediaId = await twitterClient.v1.uploadMedia(filepath, {
+                mimeType: "image/jpeg",
+            });
+            // console.log(mediaId);
+            await twitterClient.v2.tweet({
+                text: "#GIDLE #IDLE #여자아이들 #아이들 #女娃",
+                media: {
+                    media_ids: [mediaId],
+                    // get the id @G_I_DLE, https://tweeterid.com/
+                    tagged_user_ids: ["967000437797761024"],
+                },
+            });
+
+            // write the selected URI to a separate file for record-keeping
+            fs.appendFileSync("postRecord.txt", `${uri}\n`);
+        } catch (e) {
+            console.error(e);
         }
-      });
+    });
+};
 
-      // write the selected URI to a separate file for record-keeping
-      fs.appendFileSync("postRecord.txt", `${uri}\n`);
+// New function for multi-image posting with probability
+const tweetMultiple = async () => {
+    try {
+        // Determine how many images to post
+        const imageCount = getImageCount();
+        // console.log(`Posting ${imageCount} image(s)`);
 
+        // Get random images
+        const selectedImages = getRandomImages(imageCount);
+
+        // Download all images
+        const downloadedImages = await downloadImages(selectedImages);
+
+        // Upload all images to Twitter and get media IDs
+        const mediaIds = [];
+        for (const image of downloadedImages) {
+            const mediaId = await twitterClient.v1.uploadMedia(image.filepath, {
+                mimeType: "image/jpeg",
+            });
+            mediaIds.push(mediaId);
+            // console.log(`Uploaded ${image.filepath}, Media ID: ${mediaId}`);
+        }
+
+        // Create the tweet with all media
+        await twitterClient.v2.tweet({
+            text: "#GIDLE #IDLE #여자아이들 #아이들 #女娃",
+            media: {
+                media_ids: mediaIds,
+                // get the id @G_I_DLE, https://tweeterid.com/
+                tagged_user_ids: ["967000437797761024"],
+            },
+        });
+
+        // Record all posted URIs
+        const recordEntries =
+            downloadedImages.map((img) => img.uri).join("\n") + "\n";
+        fs.appendFileSync("postRecord.txt", recordEntries);
+
+        // console.log(`Successfully posted ${imageCount} image(s)`);
     } catch (e) {
-      console.error(e);
+        console.error("Error posting tweet:", e);
     }
-  });
 };
 
 // check the complete code here https://github.com/WoodyLinwc/Auto-Instagram-Bot
 const postToInsta = async () => {
-  const ig = new IgApiClient();
-  const sharp = require('sharp');
-  ig.state.generateDevice(process.env.IG_USERNAME);
-  await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-  
-  // read the uris.json file and randomly choose a image
-  const uris = JSON.parse(fs.readFileSync("uris.json", "utf8"));
-  const randomIndex = Math.floor(Math.random() * uris.length);
-  const uri = uris[randomIndex];
+    const ig = new IgApiClient();
+    const sharp = require("sharp");
+    ig.state.generateDevice(process.env.IG_USERNAME);
+    await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
 
-  const imageBuffer = await get({
-      url: uri,
-      encoding: null, 
-  });
+    // read the uris.json file and randomly choose a image
+    const uris = JSON.parse(fs.readFileSync("uris.json", "utf8"));
+    const randomIndex = Math.floor(Math.random() * uris.length);
+    const uri = uris[randomIndex];
 
-  // convert the image to acceptable format
-  const jpegBuffer = await sharp(imageBuffer).jpeg().toBuffer();
+    const imageBuffer = await get({
+        url: uri,
+        encoding: null,
+    });
 
-  await ig.publish.photo({
-      file: jpegBuffer,
-      caption: "#GIDLE #여자아이들",
-  });
+    // convert the image to acceptable format
+    const jpegBuffer = await sharp(imageBuffer).jpeg().toBuffer();
+
+    await ig.publish.photo({
+        file: jpegBuffer,
+        caption: "#GIDLE #여자아이들",
+    });
 };
 
 // tweet();
@@ -81,11 +186,7 @@ const postToInsta = async () => {
 
 // post once every 6 hours
 const cronPost = new CronJob("0 */6 * * *", async () => {
-    tweet();
+    tweetMultiple();
     // postToInsta();
 });
 cronPost.start();
-
-
-
-
