45efdf7b994696741f7e766fa355dfd6423497c2
Woody Lin <wancheng.lin001@gmail.com>
2026-06-17 16:35:24 -0400

    Update test-mastodon.js

--- test-mastodon.js
+++ test-mastodon.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+console.log("URL:", process.env.MASTODON_API_URL);
+console.log("TOKEN:", process.env.MASTODON_ACCESS_TOKEN);
 const { postToMastodon } = require("./mastodonClient");
 
 postToMastodon("测试一下 #test")
